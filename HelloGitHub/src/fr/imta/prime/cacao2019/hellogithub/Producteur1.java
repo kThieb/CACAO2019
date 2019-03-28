65911d55e2c7372dccd983a76cdea4dcca4f0ab8
Manon Geniesse <Manon Geniesse@172.16.126.249>
2019-03-28 10:37:08 +0100

    b

--- HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Producteur1.java
+++ HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Producteur1.java
@@ -5,6 +5,7 @@ public class Producteur1 {
 	public static void names() {
 		System.out.println("n18abdel");
 		System.out.println("p18lery");
+		System.out.println("m18genie");
 	}
 	
 	public static void main(String[] args) {
