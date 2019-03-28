8184b38379dbc30e040b0b8977eecfee4a8b516d
nazim iassamen <nazim.iassamen@imt-atlantique.net>
2019-03-28 10:41:58 +0100

    nazim

--- HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Grp6.java
+++ HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Grp6.java
@@ -3,6 +3,8 @@ package fr.imta.prime.cacao2019.hellogithub;
 public class Grp6 {
 	public static void main(String[] args) {
 		System.out.println("nordin git identifiant = nordinel");
+		System.out.println(" nazim git identifiant");
+		
 		
 	}
 }
