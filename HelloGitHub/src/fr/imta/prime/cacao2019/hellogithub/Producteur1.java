36e06ab55418960e3d13c1a68def7f7b52e6b745
Nassim <nassim.abdelghani@imt-atlantique.net>
2019-03-28 10:26:10 +0100

    names

---
+++ HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Producteur1.java
@@ -0,0 +1,12 @@
+package fr.imta.prime.cacao2019.hellogithub;
+
+public class Producteur1 {
+	
+	public static void names() {
+		System.out.println("n18abdel");
+	}
+	
+	public static void main(String[] args) {
+		names();
+	}
+}
