747c6cba687d4ea89a7fcd04d54c95869e4dd10e
ilias <ilias-amal-78@hotmail.fr>
2019-03-28 10:33:39 +0100

    identifiant ilias

--- HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Grp6.java
+++ HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Grp6.java
@@ -3,6 +3,7 @@ package fr.imta.prime.cacao2019.hellogithub;
 public class Grp6 {
 	public static void main(String[] args) {
 		System.out.println("nordin git identifiant = nordinel");
+		System.out.println("ilias git identifiant = ilieis");
 		
 	}
 }
