ac7810552f6937a06ccd328897e658e200e7f468
Romuald DEBRUYNE <romuald.debruyne@imt-atlantique.fr>
2019-04-25 20:34:53 +0200

    Ajout enum vis comme exemple

---
+++ CACAO2019/src/abstraction/fourni/Vis.java
@@ -0,0 +1,32 @@
+package abstraction.fourni;
+
+public enum Vis {
+	
+	V3540(3.5, 40), 
+	V3530(3.5, 30), 
+	V5060(5.0, 60);
+	
+	private double diametre;
+	private int longueur;
+	
+	Vis(double diametre, int longueur) {
+		this.diametre = diametre; 
+		this.longueur = longueur;
+	}
+	public double getDiametre() {
+		return this.diametre;
+	}
+	public int getLongueur() {
+		return this.longueur;
+	}
+	public String toString() {
+		return this.getDiametre()+"x"+this.getLongueur();
+	}
+
+	public static void main(String[] args) {
+		for (Vis v : Vis.values()) {
+			System.out.println(v);
+		}
+		System.out.println("le vis V5060 corresponde a "+Vis.V5060);
+	}
+}
