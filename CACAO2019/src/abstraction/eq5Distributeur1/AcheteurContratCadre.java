b907ba4d502aaf5d64a1f2e27844601a2cd427a6
Erine DUPONT <Erine DUPONT@172.16.123.23>
2019-05-09 11:09:18 +0200

    Implémentation automatique des fonctions AcheteurContratCadre

--- CACAO2019/src/abstraction/eq5Distributeur1/AcheteurContratCadre.java
+++ CACAO2019/src/abstraction/eq5Distributeur1/AcheteurContratCadre.java
@@ -1,5 +1,44 @@
 package abstraction.eq5Distributeur1;
 
-public class AcheteurContratCadre {
+import abstraction.eq7Romu.ventesContratCadre.ContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.IAcheteurContratCadre;
+
+public class AcheteurContratCadre implements IAcheteurContratCadre {
+
+	@Override
+	public ContratCadre getNouveauContrat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void proposerEcheancierAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void proposerPrixAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifierAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receptionner(Object produit, double quantite, ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double payer(double montant, ContratCadre cc) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
