7c45afca4234ea21934ac3cb54e08a87743d56c0
Erine DUPONT <Erine DUPONT@172.16.123.23>
2019-05-09 11:14:32 +0200

    Mise à jour

--- CACAO2019/src/abstraction/eq5Distributeur1/Distributeur1.java
+++ CACAO2019/src/abstraction/eq5Distributeur1/Distributeur1.java
@@ -1,11 +1,16 @@
 package abstraction.eq5Distributeur1;
 
+import abstraction.eq7Romu.distributionChocolat.IDistributeurChocolat;
+import abstraction.eq7Romu.produits.Chocolat;
+import abstraction.eq7Romu.ventesContratCadre.ContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.IAcheteurContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.StockEnVente;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Monde;
 
-public class Distributeur1 implements IActeur {
+public class Distributeur1 implements IActeur, IAcheteurContratCadre, IDistributeurChocolat {
 	private Journal journal;
 
 	public Distributeur1() {
@@ -21,5 +26,67 @@ public class Distributeur1 implements IActeur {
 	}
 
 	public void next() {
+	}
+
+// ------------------------------------------------------------------------------------------------------
+// ACHETEUR
+// ------------------------------------------------------------------------------------------------------ 
+	
+	@Override
+	public ContratCadre getNouveauContrat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void proposerEcheancierAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void proposerPrixAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifierAcheteur(ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receptionner(Object produit, double quantite, ContratCadre cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double payer(double montant, ContratCadre cc) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+// ---------------------------------------------------------------------------------------------------------
+// VENDEUR CLIENT
+// ---------------------------------------------------------------------------------------------------------
+	
+	@Override
+	public StockEnVente<Chocolat> getStockEnVente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getPrix(Chocolat c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double vendre(Chocolat c, double quantite) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
