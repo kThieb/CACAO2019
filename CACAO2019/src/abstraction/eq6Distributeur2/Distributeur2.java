3c5cb7edb2628f1bcdb4ecaf3b0d1433b38727f9
caro <caro@dhcp-3-100.gde.emn.fr>
2019-05-09 10:32:31 +0200

    interface

--- CACAO2019/src/abstraction/eq6Distributeur2/Distributeur2.java
+++ CACAO2019/src/abstraction/eq6Distributeur2/Distributeur2.java
@@ -1,11 +1,16 @@
 package abstraction.eq6Distributeur2;
 
+import abstraction.eq7Romu.distributionChocolat.IDistributeurChocolat;
+import abstraction.eq7Romu.produits.Chocolat;
+import abstraction.eq7Romu.ventesContratCadre.ContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.IAcheteurContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.StockEnVente;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Monde;
 
-public class Distributeur2 implements IActeur {
+public class Distributeur2 implements IActeur, IAcheteurContratCadre<Chocolat>, IDistributeurChocolat {
 
 	
 	private Journal journal;
@@ -23,5 +28,59 @@ public class Distributeur2 implements IActeur {
 	}
 
 	public void next() {
+	}
+
+	@Override
+	public StockEnVente<Chocolat> getStockEnVente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getPrix(Chocolat c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double vendre(Chocolat c, double quantite) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ContratCadre<Chocolat> getNouveauContrat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void proposerEcheancierAcheteur(ContratCadre<Chocolat> cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void proposerPrixAcheteur(ContratCadre<Chocolat> cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifierAcheteur(ContratCadre<Chocolat> cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receptionner(Chocolat produit, double quantite, ContratCadre<Chocolat> cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double payer(double montant, ContratCadre<Chocolat> cc) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
