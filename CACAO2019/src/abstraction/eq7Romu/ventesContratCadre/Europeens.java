bd505614bc6c7c11abe415a8def4d366b664437d
nazim iassamen <nazim.iassamen@imt-atlantique.net>
2019-05-16 11:37:50 +0200

    changement client

---
+++ CACAO2019/src/abstraction/eq7Romu/ventesContratCadre/Europeens.java
@@ -0,0 +1,11 @@
+package abstraction.eq7Romu.ventesContratCadre;
+
+public class Europeens extends Client1 {
+
+	public Europeens(int numero, int noteprix, int notequalite, int notequantite) {
+		super(numero, noteprix, notequalite, notequantite);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	}
