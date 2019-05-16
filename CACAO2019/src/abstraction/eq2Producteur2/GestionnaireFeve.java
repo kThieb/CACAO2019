a2672579137e8a300bb3f98b2e5994b3e39fdac8
DELL <DELL@172.16.122.142>
2019-05-16 11:25:19 +0200

    HashMap

--- CACAO2019/src/abstraction/eq2Producteur2/GestionnaireFeve.java
+++ CACAO2019/src/abstraction/eq2Producteur2/GestionnaireFeve.java
@@ -3,11 +3,16 @@ package abstraction.eq2Producteur2;
 import java.util.HashMap;
 
 import abstraction.eq7Romu.produits.Feve;
+import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
 
 public class GestionnaireFeve {
 	private HashMap<Feve, Indicateur> feves=new HashMap<Feve,Indicateur>();
 	
-	public GestionnaireFeve() {
+	public GestionnaireFeve(IActeur acteur) {
+		feves.put(Feve.FORASTERO_MG_NEQ, new Indicateur(Feve.FORASTERO_MG_NEQ.name(), acteur,0));
+		feves.put(Feve.FORASTERO_MG_EQ, new Indicateur(Feve.FORASTERO_MG_EQ.name(),acteur,0));
+		feves.put(Feve.MERCEDES_MG_EQ, new Indicateur(Feve.MERCEDES_MG_EQ.name(),acteur,0));
+		feves.put(Feve.MERCEDES_MG_NEQ, new Indicateur(Feve.MERCEDES_MG_NEQ.name(),acteur,0));
 	}
 }
