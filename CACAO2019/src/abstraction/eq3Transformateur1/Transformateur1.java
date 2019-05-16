9047a97b2b56119934242360e4d23d3664ff0333
bentolila0 <eve.ben-tolila@imt-atlantique.net>
2019-05-16 09:54:00 +0200

    Initialisation des stocks (pas fini), Stock en double au lieu de int

--- CACAO2019/src/abstraction/eq3Transformateur1/Transformateur1.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -39,14 +39,23 @@ public class Transformateur1 implements IActeur, IAcheteurContratCadre<Feve>, IV
 	
 	public Transformateur1() {
 		
-		// begin eve
+		// --------------------------------- begin eve
+		
+		// stock de feves
 		this.stockFeves = new HashMap<Feve,Stock>();
+		this.stockFeves.put(Feve.CRIOLLO_HG_EQ, new Stock(0));
+		
+		// stock de chocolat
 		this.stockChocolat = new HashMap<Chocolat,Stock>();
+		this.stockChocolat.put(Chocolat.MG_NE_HP, new Stock(0));
+		this.stockChocolat.put(Chocolat.MG_NE_SHP, new Stock(0));
+		this.stockChocolat.put(Chocolat.MG_E_SHP, new Stock(0));
+		
 //		int sommeFeves = 0;
 //		this.iStockFeves = new Indicateur("EQ3 stock feves", this, sommeFeves);
 //		int sommeChocolat = 0;
 //		this.iStockChocolat = new Indicateur("EQ3 stock chocolat", this, sommeChocolat);
-		// end eve
+		// --------------------------------- end eve
 		this.soldeBancaire=new Indicateur("EQ3 solde bancaire", this, 100000);
 		this.journal = new Journal ("Vente aléatoire de cacao");
 		Monde.LE_MONDE.ajouterJournal(this.journal);
