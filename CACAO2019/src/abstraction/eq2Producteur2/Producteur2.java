fa80be0932c01f9ed744c5c7f66527ec9e74b54b
DELL <DELL@172.16.122.142>
2019-05-16 10:07:56 +0200

    ProposerPrixVendeur ProposerEcheancierDendeur

--- CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
+++ CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
@@ -92,18 +92,30 @@ public class Producteur2 implements IActeur, IVendeurCacaoAleatoire, IVendeurCon
 
 
 
-	@Override
+	/** a modifier*/
 	public void proposerEcheancierVendeur(ContratCadre<Feve> cc) {
-		if (Math.random()<0.5) { // une chance sur deux d'accepter l'echeancier
-			cc.ajouterEcheancier(new Echeancier(cc.getEcheancier())); // on accepte la proposition de l'acheteur car on a la quantite en stock 
-		} else { // une chance sur deux de proposer un echeancier etalant sur un step de plus
-			cc.ajouterEcheancier(new Echeancier(cc.getEcheancier().getStepDebut(), cc.getEcheancier().getNbEcheances()+1, cc.getQuantite()/(cc.getEcheancier().getNbEcheances()+1)));
-		}
+		cc.ajouterEcheancier(new Echeancier(cc.getEcheancier())); // on accepte la proposition de l'acheteur car on a la quantite en stock 
 	}
 		
 
 	@Override
 	public void proposerPrixVendeur(ContratCadre<Feve> cc) {
+		if (cc.getListePrixAuKilo().size()==0) {
+			cc.ajouterPrixAuKilo(getPrix(cc.getProduit(), cc.getQuantite()));
+		} else {
+			double prixVendeur = cc.getListePrixAuKilo().get(0);
+			double prixAcheteur = cc.getPrixAuKilo();
+			cc.ajouterPrixAuKilo(prixVendeur);
+			
+			
+			/*if (prixAcheteur>=0.75*prixVendeur) { // on ne fait une proposition que si l'acheteur ne demande pas un prix trop bas.
+				if (Math.random()<0.25) { // probabilite de
+					cc.ajouterPrixAuKilo(cc.getPrixAuKilo());
+				} else {
+					cc.ajouterPrixAuKilo((prixVendeur*(0.9+Math.random()*0.1))); // rabais de 10% max
+				}
+			}*/
+		}
 	}
 
 	@Override
--- HelloGitHub/src/fr/imta/prime/cacao2019/hellogithub/Eq7Romu.java
+++
@@ -1,8 +0,0 @@
-package fr.imta.prime.cacao2019.hellogithub;
-
-public class Eq7Romu {
-	public static void main(String[] args) {
-		System.out.println("Romu");
-	}
-
-}
