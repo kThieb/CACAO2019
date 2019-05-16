535cf336cfc1583a7a0ca0990d3d2bf7a5b1d9e4
Nassim <nassim.abdelghani@imt-atlantique.net>
2019-05-16 10:00:45 +0200

    méthodes vides vendeurcontratcadre

--- CACAO2019/src/abstraction/eq1Producteur1/VendeurContratCadre.java
+++ CACAO2019/src/abstraction/eq1Producteur1/VendeurContratCadre.java
@@ -6,7 +6,7 @@ import abstraction.eq7Romu.ventesContratCadre.IVendeurContratCadre;
 import abstraction.eq7Romu.ventesContratCadre.StockEnVente;
 
 public class VendeurContratCadre implements IVendeurContratCadre {
-	//ANTI 
+	// ANTI
 
 	@Override
 	public StockEnVente getStockEnVente() {
@@ -25,36 +25,41 @@ public class VendeurContratCadre implements IVendeurContratCadre {
 		// Nassim
 		cc.ajouterEcheancier(new Echeancier(cc.getEcheancier()));
 	}
+
 //Manon
 	@Override
 	public void proposerPrixVendeur(ContratCadre cc) {
-		/*Si la liste est nulle on ajoute le prix initialement proposé*/
-		if(cc.getListePrixAuKilo()==null) {
+		/* Si la liste est nulle on ajoute le prix initialement proposé */
+		if (cc.getListePrixAuKilo() == null) {
 			cc.ajouterPrixAuKilo(this.getPrix(cc.getProduit(), cc.getQuantite()));
-		}
-		else {
+		} else {
 			double prixVendeur = (double) cc.getListePrixAuKilo().get(-2);
-			double prixAcheteur =cc.getPrixAuKilo();
-			/*Si la différence de prix est inférieur à celle de 5% proposé on accepte le prix de l'acheteur*/
-			if (prixVendeur-prixAcheteur<0.05*prixVendeur){
-			cc.ajouterPrixAuKilo(prixAcheteur);
-		}
-			/* Sinon on propose in prix inferieur mais superieur à la moyenne des deux prix*/
-			else{
+			double prixAcheteur = cc.getPrixAuKilo();
+			/*
+			 * Si la différence de prix est inférieur à celle de 5% proposé on accepte le
+			 * prix de l'acheteur
+			 */
+			if (prixVendeur - prixAcheteur < 0.05 * prixVendeur) {
+				cc.ajouterPrixAuKilo(prixAcheteur);
+			}
+			/*
+			 * Sinon on propose in prix inferieur mais superieur à la moyenne des deux prix
+			 */
+			else {
 				double nouveauPrix;
-				nouveauPrix= prixVendeur-(prixVendeur-prixAcheteur)*0.2;
+				nouveauPrix = prixVendeur - (prixVendeur - prixAcheteur) * 0.2;
 				cc.ajouterPrixAuKilo(nouveauPrix);
-				
-			}}
-			
-		
-		
+
+			}
+		}
+
 	}
+
 //END
 	@Override
 	public void notifierVendeur(ContratCadre cc) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -66,7 +71,7 @@ public class VendeurContratCadre implements IVendeurContratCadre {
 	@Override
 	public void encaisser(double montant, ContratCadre cc) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
