b38c5df71cf7cc1cb7e55faac1a09df5b52e2e69
bentolila0 <eve.ben-tolila@imt-atlantique.net>
2019-05-23 10:25:03 +0200

    Nouvelles methodes dans stock

--- CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
@@ -19,8 +19,15 @@ public class Stock<T> {
 	public double getQuantiteEnStock(T produit) {
 		return this.stock.get(produit);
 	}
-	public HashMap<T, Double> getToutStock() {
-		return this.stock;
+	public boolean estEnStock(T produit) {
+		return this.stock.containsKey(produit);
+	}
+	public ArrayList<T> getProduitsEnStock() {
+		ArrayList<T> resultat = new ArrayList<T>();
+		for (T p: this.stock.keySet()) {
+			resultat.add(p);
+		}
+		return resultat;
 	}
 	public void setQuantiteEnStock(T produit, double quantite) {
 		if (quantite >= 0.) { this.stock.put(produit, quantite); }
