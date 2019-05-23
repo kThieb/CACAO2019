7e4d1d29a71cee1d5e32c417b50cd9b5fa2064d4
bentolila0 <eve.ben-tolila@imt-atlantique.net>
2019-05-23 10:05:20 +0200

    Modification des stocks ; besoin de modifier les fonctions suivantes

--- CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
@@ -1,24 +1,27 @@
 package abstraction.eq3Transformateur1;
 
-public class Stock {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	private double quantite;
+public class Stock<T> {
+
+	private HashMap<T, Double> stock;
 	
-	public Stock(double quantite) {
-		this.quantite = quantite;
+	public Stock(ArrayList<T> produits) {
+		for (T p: produits) { this.stock.put(p, 0.); }
 	}
-	public Stock() {
-		this.quantite = 0;
-	}
+	public Stock() { }
 	
-	public double getQuantiteEnStock() {
-		return this.quantite;
+	// -----------------------------------------------------------
+	//          GETTERS & SETTERS
+	// -----------------------------------------------------------
+	
+	public double getQuantiteEnStock(T produit) {
+		return this.stock.get(produit);
 	}
-	public void setQuantiteEnStock(double quantite) {
-		this.quantite = quantite;
-	}
-	public void addEnStock(double quantite) {
-		this.quantite = this.quantite + quantite;
+	public void setQuantiteEnStock(T produit, double quantite) {
+		if (quantite >= 0.) { this.stock.put(produit, quantite); }
+		else { this.stock.put(produit, 0.); }
 	}
 	
 }
