38349e75f858e91cb3e43a97c3a0ae36f0edbf53
Erine DUPONT <Erine DUPONT@172.16.123.23>
2019-05-23 11:52:02 +0200

    Mise à jour

--- CACAO2019/src/abstraction/eq5Distributeur1/Distributeur1.java
+++ CACAO2019/src/abstraction/eq5Distributeur1/Distributeur1.java
@@ -155,15 +155,27 @@ public class Distributeur1 implements IActeur, IAcheteurContratCadre, IDistribut
 		}
 	}
 
+	/**
+	 * @author Erine DUPONT 
+	 */
 	@Override
-	public double vendre(Chocolat c, double quantite) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double vendre(Chocolat chocolat, double quantite) {
+		double stock = this.getStockEnVente().get(chocolat);
+		if (quantite < 0.0) {
+			throw new IllegalArgumentException("Appel de vendre(chocolat, quantité) de "
+					+ "Distributeur1 avec quantité<0.0 (=="+quantite+")");
+		} else if (stock < quantite) {
+			throw new IllegalArgumentException("Appel de vendre(chocolat, quantité) de "
+					+ "Distributeur1 avec stock ( ==" + stock +") < quantité (=="+quantite+")");
+		} else {
+			this.getStockEnVente().set(chocolat, stock - quantite);
+			return stock - quantite;
+		}
 	}
 
 
 	/**
-	 * Erwann DEFOY
+	 * @author Erwann DEFOY
 	 */
 	@Override
 	public double getNoteQualite(Chocolat c) {
