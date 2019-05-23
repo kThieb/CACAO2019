c83a3f9dab76b7c27dabf81bf471524f80b2c48c
bentolila0 <eve.ben-tolila@imt-atlantique.net>
2019-05-23 11:43:50 +0200

    ajout du cout en feves de chaque produit

--- CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
@@ -3,12 +3,18 @@ package abstraction.eq3Transformateur1;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import abstraction.eq7Romu.produits.Feve;
+
 public class Stock<T> {
 
-	private HashMap<T, Double> stock;
+	private HashMap<T, HashMap<String, Double>> stock;
 	
 	public Stock(ArrayList<T> produits) {
-		for (T p: produits) { this.stock.put(p, 0.); }
+		for (T p: produits) { 
+			this.stock.put(p, new HashMap<String, Double>()); 
+			this.stock.get(p).put("quantite", 0.);
+			this.stock.get(p).put("cout en feves", 0.);
+		}
 	}
 	public Stock() { }
 	
@@ -17,10 +23,10 @@ public class Stock<T> {
 	// -----------------------------------------------------------
 	
 	public double getQuantiteEnStock(T produit) {
-		return this.stock.get(produit);
+		return this.stock.get(produit).get("quantite");
 	}
 	public boolean estEnStock(T produit) {
-		return this.stock.containsKey(produit);
+		return this.stock.containsKey(produit) && (this.stock.get(produit).get("quantite") > 0.);
 	}
 	public ArrayList<T> getProduitsEnStock() {
 		ArrayList<T> resultat = new ArrayList<T>();
@@ -29,11 +35,25 @@ public class Stock<T> {
 		}
 		return resultat;
 	}
+	public double getCoutEnFeves(T produit) throws IllegalArgumentException {
+		if (produit instanceof Feve) {
+			if (estEnStock(produit)) {
+				double cout = this.stock.get(produit).get("cout en feves");
+				return cout;
+			}
+			else { throw new IllegalArgumentException("La feve demandee n'est pas en stock"); }
+		}
+		else { throw new IllegalArgumentException("Le produit passe en argument n'est pas une feve"); }
+	}
 	
 	
 	public void setQuantiteEnStock(T produit, double quantite) {
-		if (quantite >= 0.) { this.stock.put(produit, quantite); }
-		else { this.stock.put(produit, 0.); }
+		if (quantite >= 0.) { this.stock.get(produit).put("quantite", quantite); }
+		else { this.stock.get(produit).put("quantite", 0.); }
+	}
+	public void setCoutEnFeves(T produit, double cout) {
+		if (cout >= 0.) { this.stock.get(produit).put("cout en feves", cout); }
+		else { this.stock.get(produit).put("cout en feves", 0.); }
 	}
 	
 }
--- CACAO2019/src/abstraction/eq3Transformateur1/Transformateur1.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -64,8 +64,11 @@ public class Transformateur1 implements IActeur, IAcheteurContratCadre<Feve>, IV
 		// stock de chocolat
 		this.stockChocolat = new Stock<Chocolat>();
 		this.stockChocolat.setQuantiteEnStock(Chocolat.MG_NE_HP, 0.);
+		this.stockChocolat.setCoutEnFeves(Chocolat.MG_NE_HP, 0.1);
 		this.stockChocolat.setQuantiteEnStock(Chocolat.MG_NE_SHP, 0.);
+		this.stockChocolat.setCoutEnFeves(Chocolat.MG_NE_SHP, 0.075);
 		this.stockChocolat.setQuantiteEnStock(Chocolat.MG_E_SHP, 0.);
+		this.stockChocolat.setCoutEnFeves(Chocolat.MG_E_SHP, 0.120);
 		
  		this.iStockFeves = new Indicateur("EQ3 stock feves", this, 0);
  		this.iStockChocolat = new Indicateur("EQ3 stock chocolat", this, 0);
@@ -117,6 +120,11 @@ public class Transformateur1 implements IActeur, IAcheteurContratCadre<Feve>, IV
 		// TODO pas fini encore
 		// transformer les differents produits
 		// updater les indicateurs, stocks et solde bancaire
+		
+		// les quantites de cacao utilisees sont celles specifiees dans le cahier des charges v2
+		ArrayList<Double> quantitesTransformees = new ArrayList<Double>();
+		//for ()
+		
 		double quantiteTransformee = Math.random()*Math.min(100, this.iStockFeves.getValeur()); // on suppose qu'on a un stock infini de sucre
 		this.iStockFeves.retirer(this, quantiteTransformee);
 		this.iStockChocolat.ajouter(this, (2*quantiteTransformee));// 50% cacao, 50% sucre
