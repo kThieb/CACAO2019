b2749b625fc55806dc1070a4cfe2d36a045290e0
elsaf <elsaf@DESKTOP-LIUDN8G>
2019-05-27 09:43:01 +0200

    modif proposer echeancier

--- CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
+++ CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
@@ -116,7 +116,7 @@ public class Producteur2 implements IActeur, IVendeurContratCadre<Feve> {
 			if (e.getQuantiteTotale() > this.getStockEnVente().get(cc.getProduit())) { //On s assure que la quantitée demandée est en stock
 				throw new IllegalArgumentException("La quantité demandée n est pas disponible");
 			} else {
-				cc.ajouterEcheancier(new Echeancier(cc.getEcheancier())); // on accepte la proposition de l'acheteur car on a la quantite en stock 
+				cc.ajouterEcheancier(new Echeancier(e)); // on accepte la proposition de l'acheteur car on a la quantite en stock 
 			}
 		}
 	}
