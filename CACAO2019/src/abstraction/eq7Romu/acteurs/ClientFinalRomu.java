df518c1dc1ddd439782e84b899edcb65ff193a0c
nordinel <nordin.el-madraj@imt-atlantique.net>
2019-05-28 20:55:11 +0200

    problème de ddddd

--- CACAO2019/src/abstraction/eq7Romu/acteurs/ClientFinalRomu.java
+++ CACAO2019/src/abstraction/eq7Romu/acteurs/ClientFinalRomu.java
@@ -33,7 +33,6 @@ public class ClientFinalRomu implements IActeur {
 	}
 
 	public void next() {
-		System.out.println("dddddd");
 		this.journal.ajouter("Step "+Monde.LE_MONDE.getStep()+" : tentative d'achat de "+quantiteParStep+" de "+this.uniqueProduit+" ____________");
 		double quantiteAchetee = 0.0;
 		IDistributeurChocolat distributeurLeMoinsCher = null;
