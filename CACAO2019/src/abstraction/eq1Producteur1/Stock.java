20c161e1f6730fb4d129e56f0cbc273443d52041
Nassim <nassim.abdelghani@imt-atlantique.net>
2019-06-06 15:11:35 +0200

    stock

--- CACAO2019/src/abstraction/eq1Producteur1/Stock.java
+++ CACAO2019/src/abstraction/eq1Producteur1/Stock.java
@@ -3,32 +3,41 @@ package abstraction.eq1Producteur1;
 import java.util.HashMap;
 
 import abstraction.eq7Romu.produits.Feve;
+import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
+import static abstraction.eq1Producteur1.Producteur1Interne.*;
 
 public class Stock{
 	private Indicateur ind;
 	private HashMap<Integer, Integer> stock;
 	private int alea;
+	private IActeur act;
+	private int nextBorneInf=-(40-1)*unAnEnSteps;
 	
 	
-	public Stock(Feve feve) {
-		ind=new Indicateur("EQ1 stock "+feve.getVariete(), new Producteur1(), 1000);
+	public Stock(Feve feve,IActeur act) {
+		this.act=act;
+		ind=new Indicateur("EQ1 stock "+feve.getVariete(), act, 1000);
 		stock=new HashMap<Integer, Integer>();
-		//for ()
+		for (int an=0;an<40;an++) {
+			stock.put(-an*unAnEnSteps, 1000/40);
+		}
 		
 	}
 	
 	public void depot(int next,int quantite) {
 		stock.put(next, quantite);
+		ind.ajouter(act, quantite);
 	}
 	
 	public int retrait(int nextCourant,int quantite) {
 		int quantiteAEnlever=quantite;
-		int nextAExplorer=0;
+		int nextAExplorer=nextBorneInf;
 		while (quantiteAEnlever>0 && nextAExplorer <=nextCourant) {
 			if (stock.get(nextAExplorer)<quantiteAEnlever) {
 				quantiteAEnlever=quantiteAEnlever-stock.get(nextAExplorer);
 				stock.put(nextAExplorer, 0);
+				nextBorneInf=nextAExplorer;
 			} else {
 			
 				stock.put(nextAExplorer, stock.get(nextAExplorer)-quantiteAEnlever);
@@ -37,17 +46,19 @@ public class Stock{
 			nextAExplorer++;
 			
 		}
-		return quantite-quantiteAEnlever;
+		int quantiteRetire=quantite-quantiteAEnlever;
+		ind.retirer(act, quantiteRetire);
+		return quantiteRetire;
 		
 	}
 	
 	public void updateStock(int nextCourant,int recolte) {
 		stock.put(nextCourant,recolte);
-		int nextPerime=nextCourant-Producteur1Interne.dureeDeVieFeve;
+		int nextPerime=nextCourant-dureeDeVieFeve;
 		int stockPerime=stock.get(nextPerime);
 		stock.put(nextPerime, 0);
-		ind.retirer(new Producteur1(), stockPerime);
-		ind.ajouter(new Producteur1(), recolte);
+		ind.retirer(act, stockPerime);
+		ind.ajouter(act, recolte);
 	}
 	
 	public static void main(String[] args) {
