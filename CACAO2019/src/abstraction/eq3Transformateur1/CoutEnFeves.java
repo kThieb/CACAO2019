c20663c0354d5dc3b2a1246e822ce850cf8f8c81
Clément Magnin <Clementmagnin@outlook.com>
2019-06-06 09:58:26 +0200

    Replaced with upstream

---
+++ CACAO2019/src/abstraction/eq3Transformateur1/CoutEnFeves.java
@@ -0,0 +1,54 @@
+package abstraction.eq3Transformateur1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import abstraction.eq7Romu.produits.Chocolat;
+import abstraction.eq7Romu.produits.Feve;
+
+public class CoutEnFeves {
+	
+	private HashMap<Chocolat, HashMap<Feve, Double>> cout;
+	
+	public CoutEnFeves() {
+	}
+	
+	public CoutEnFeves(ArrayList<Chocolat> chocolats, ArrayList<Feve> feves) {
+		this.cout = new HashMap<Chocolat, HashMap<Feve, Double>>();
+		for (Chocolat c: chocolats) { 
+			this.cout.put(c, new HashMap<Feve, Double>());
+			for(Feve f:feves) {
+				this.cout.get(c).put(f, 0.);
+			}	
+		}
+	}
+	
+	// -----------------------------------------------------------
+	//          GETTERS
+	// -----------------------------------------------------------
+	
+	public double getCoutEnFeves(Chocolat chocolat, Feve feve) {
+		if(!this.cout.containsKey(chocolat)) {
+			return 0.;
+		}else if(!this.cout.get(chocolat).containsKey(feve)){
+			return 0.;
+		}else {
+			return this.cout.get(chocolat).get(feve);
+		}
+	}
+
+	// -----------------------------------------------------------
+	//          SETTERS
+	// -----------------------------------------------------------
+	
+	public void setCoutEnFeves(Chocolat chocolat, Feve feve, double cout) {
+		if(!this.cout.containsKey(chocolat)) {
+			this.cout.put(chocolat, new HashMap<Feve, Double>());
+		}
+		if (cout >= 0.) {
+			this.cout.get(chocolat).put(feve, cout);
+		} else {
+			this.cout.get(chocolat).put(feve, 0.);
+		}
+	}
+}
--- CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
+++ CACAO2019/src/abstraction/eq3Transformateur1/Stock.java
@@ -1,24 +1,49 @@
 package abstraction.eq3Transformateur1;
 
-public class Stock {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	private double quantite;
+public class Stock<T> {
+
+	// T est le chocolat ou la feve ; lui est associe une quantite en stock, en kg
+	private HashMap<T, Double> stock;
 	
-	public Stock(double quantite) {
-		this.quantite = quantite;
+	public Stock(ArrayList<T> produits) {
+		this.stock = new HashMap<T, Double>();
+		for (T p: produits) { 
+			this.stock.put(p, 0.);
+		}
 	}
-	public Stock() {
-		this.quantite = 0;
+	public Stock() { }
+	
+	// -----------------------------------------------------------
+	//          GETTERS
+	// -----------------------------------------------------------
+	
+	public double getQuantiteEnStock(T produit) {
+		return this.stock.get(produit);
 	}
 	
-	public double getQuantiteEnStock() {
-		return this.quantite;
-	}
-	public void setQuantiteEnStock(double quantite) {
-		this.quantite = quantite;
-	}
-	public void addEnStock(double quantite) {
-		this.quantite = this.quantite + quantite;
+	public boolean estEnStock(T produit) {
+		return this.stock.containsKey(produit) && (this.stock.get(produit) > 0.);
 	}
 	
+	public ArrayList<T> getProduitsEnStock() {
+		ArrayList<T> resultat = new ArrayList<T>();
+		for (T p: this.stock.keySet()) {
+			resultat.add(p);
+		}
+		return resultat;
+	}
+	
+	
+	// -----------------------------------------------------------
+	//          SETTERS
+	// -----------------------------------------------------------
+	
+	public void setQuantiteEnStock(T produit, double quantite) {
+		if (quantite >= 0.) { this.stock.put(produit, quantite); }
+		else { this.stock.put(produit, 0.); }
+	}
+
 }
