b1152bb7235cf0b67eeaf333fec6daf03609fa51
caro <caro@dhcp-3-100.gde.emn.fr>
2019-06-13 11:04:17 +0200

    chocolat

--- CACAO2019/src/abstraction/eq7Romu/acteurs/CreateurRomu.java
+++ CACAO2019/src/abstraction/eq7Romu/acteurs/CreateurRomu.java
@@ -22,11 +22,11 @@ public class CreateurRomu implements IActeur {
 		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.TRINITARIO_MG_NEQ,
 				15000, 5000.0, 100000.0));
 		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.TRINITARIO_MG_NEQ, 
-				Chocolat.MG_NE_HP, 10000, 1.5, 5000.0, 5000.0, 100000.0, 0.15));
+				Chocolat.MG_NE_HP, 30000, 1.5, 5000.0, 5000.0, 100000.0, 0.15));
 		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.TRINITARIO_MG_NEQ, 
-				Chocolat.MG_NE_HP, 5000, 1.8, 5000.0, 5000.0, 100000.0, 0.20));
+				Chocolat.MG_NE_HP, 30000, 1.8, 5000.0, 5000.0, 100000.0, 0.20));
 		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.CRIOLLO_HG_EQ, 
-				Chocolat.HG_E_SHP, 2000, 1.1, 2000.0, 2000.0, 100000.0, 0.25));
+				Chocolat.HG_E_SHP, 30000, 1.1, 2000.0, 2000.0, 100000.0, 0.25));
 //		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.MG_NE_HP, 
 //				0.25, 200.0, 100000.0));
 		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.HG_E_SHP, 
