70b63007fcf199e0001e9b814df05ce9d3cdcd70
antineajouannis <antinea.jouannis@imt-atlantique.net>
2019-06-13 10:14:42 +0200

    xcv

--- CACAO2019/src/abstraction/eq1Producteur1/Plantation.java
+++ CACAO2019/src/abstraction/eq1Producteur1/Plantation.java
@@ -1,17 +1,28 @@
 package abstraction.eq1Producteur1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import abstraction.eq7Romu.produits.Feve;
+import abstraction.eq7Romu.produits.Variete;
+import abstraction.eq7Romu.ventesContratCadre.ContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.Echeancier;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
 import static abstraction.eq1Producteur1.Producteur1Interne.*;
+
+
 
 public class Plantation {
 	private Indicateur ind;
 	private HashMap<Integer, Double> plantation;//clé=step de stockage, objet=quantité
 	private IActeur act;
 	private int stepBorneInf=-(40-1)*unAnEnSteps;
+	protected HashMap<Integer, ContratCadre<Feve>> historiqueContrats;
 	
 	public Plantation(Feve feve,IActeur act,int plantationDepart) {
 		this.act=act;
@@ -61,10 +72,110 @@ public class Plantation {
 			getInd().retirer(getAct(), ArbresAges);
 		} 
 	}
-	
-	public Double moyenneDemande(){
-		return Double.NaN;
+	public Double moyenneDemandeCriollo(){
+		Double moyenne = 0.0;
+		Set<Entry<Integer, ContratCadre<Feve>>> setHisto= historiqueContrats.entrySet();
+		Iterator<Entry<Integer, ContratCadre<Feve>>> it = setHisto.iterator();
+		while(it.hasNext()) {
+			Entry<Integer, ContratCadre<Feve>> e = it.next();
+			if (e.getValue().getProduit().getVariete() == Variete.CRIOLLO) {
+			List<Echeancier> echeanciers = e.getValue().getEcheanciers();
+			
+			for(int i=0; i<echeanciers.size(); i++) {
+				Echeancier echeancier = echeanciers.get(i);
+				int stepDebut = echeancier.getStepDebut();
+				if (stepDebut <cinqAnsEnSteps) {
+					int stepFin = echeancier.getStepFin();
+					if (stepFin > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps;
+					}
+					moyenne += echeancier.getQuantiteJusquA(stepFin);
+					if (stepFin - stepDebut > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps; }
+			
+		}
+		
 	}
+		}
+		 
+		}return moyenne / 5 ;}
+	
+	public Double moyenneDemandeForastero(){
+		Double moyenne = 0.0;
+		Set<Entry<Integer, ContratCadre<Feve>>> setHisto= historiqueContrats.entrySet();
+		Iterator<Entry<Integer, ContratCadre<Feve>>> it = setHisto.iterator();
+		while(it.hasNext()) {
+			Entry<Integer, ContratCadre<Feve>> e = it.next();
+			if (e.getValue().getProduit().getVariete() == Variete.CRIOLLO) {
+			List<Echeancier> echeanciers = e.getValue().getEcheanciers();
+			
+			for(int i=0; i<echeanciers.size(); i++) {
+				Echeancier echeancier = echeanciers.get(i);
+				int stepDebut = echeancier.getStepDebut();
+				if (stepDebut <cinqAnsEnSteps) {
+					int stepFin = echeancier.getStepFin();
+					if (stepFin > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps;
+					}
+					moyenne += echeancier.getQuantiteJusquA(stepFin);
+					if (stepFin - stepDebut > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps; }
+			
+		}
+		
+	}}
+			 }
+		return moyenne/5 ;}
+	
+	public Double moyenneDemandeTrinitario(){
+		Double moyenne = 0.0;
+		Set<Entry<Integer, ContratCadre<Feve>>> setHisto= historiqueContrats.entrySet();
+		Iterator<Entry<Integer, ContratCadre<Feve>>> it = setHisto.iterator();
+		while(it.hasNext()) {
+			Entry<Integer, ContratCadre<Feve>> e = it.next();
+			if (e.getValue().getProduit().getVariete() == Variete.CRIOLLO) {
+			List<Echeancier> echeanciers = e.getValue().getEcheanciers();
+			
+			for(int i=0; i<echeanciers.size(); i++) {
+				Echeancier echeancier = echeanciers.get(i);
+				int stepDebut = echeancier.getStepDebut();
+				if (stepDebut <cinqAnsEnSteps) {
+					int stepFin = echeancier.getStepFin();
+					if (stepFin > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps;
+					}
+					moyenne += echeancier.getQuantiteJusquA(stepFin);
+					if (stepFin - stepDebut > cinqAnsEnSteps) {
+						stepFin = cinqAnsEnSteps; }
+			
+		}
+		
+	}
+				}
+				
+				
+			}
+		return moyenne/5 ; 
+		
+			
+			
+		}
+		
+	
+		
+	public Double moyenneDemande(Feve feve ){
+		if (feve.getVariete()== Variete.CRIOLLO) {
+			return this.moyenneDemandeCriollo();
+		      }
+		if (feve.getVariete()== Variete.FORASTERO) {
+			return this.moyenneDemandeForastero();
+		      }
+		if (feve.getVariete()== Variete.TRINITARIO) {
+			return this.moyenneDemandeTrinitario();
+		      }
+			return 0.0;
+		}
+	
 	
 	public void updatePlantation(int stepCourant,double plantation) {
 		planter(stepCourant,plantation); 
