44bf9baeb06149ada5b3672f9b2b93d0dcdc7039
Clément Magnin <Clementmagnin@outlook.com>
2019-06-13 08:27:55 +0200

    change prod 2

--- CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
+++ CACAO2019/src/abstraction/eq2Producteur2/Producteur2.java
@@ -120,7 +120,8 @@ public void recolte(Feve f) {
 			double qualitePRoduction=maladie_predateurs+meteo;
 			//double qualiteProduction = (Math.random() - 0.5) / 2.5 + 1; // entre 0.8 et 1.2
 			double nouveauStock = this.gestionnaireFeve.getStock(f)
-						+ this.gestionnaireFeve.getProductionParStep(f) * (1 + qualitePRoduction); 
+						+ this.gestionnaireFeve.getProductionParStep(f) * (1 + qualitePRoduction);
+			
 			this.gestionnaireFeve.setStock(this, f, nouveauStock);}}
 
 
