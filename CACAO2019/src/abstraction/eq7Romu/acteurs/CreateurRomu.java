b31cc0fe313d7e81b1da5751038dbdb72ecf3a24
Clément Magnin <Clementmagnin@outlook.com>
2019-06-13 11:02:31 +0200

    replace createurromu / monde

--- CACAO2019/src/abstraction/eq7Romu/acteurs/CreateurRomu.java
+++ CACAO2019/src/abstraction/eq7Romu/acteurs/CreateurRomu.java
@@ -15,10 +15,10 @@ public class CreateurRomu implements IActeur {
 
 	public CreateurRomu() {
 		Monde.LE_MONDE.ajouterActeur(new SuperviseurVentesContratCadre());
-//		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.CRIOLLO_HG_EQ,
-//				10000, 100000.0, 100000.0));
-//		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.CRIOLLO_HG_EQ,
-//				10000, 100000.0, 100000.0));
+		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.CRIOLLO_HG_EQ,
+				10000, 100000.0, 100000.0));
+		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.CRIOLLO_HG_EQ,
+				10000, 100000.0, 100000.0));
 //		Monde.LE_MONDE.ajouterActeur(new ProducteurRomu(Feve.TRINITARIO_MG_NEQ,
 //				15000, 5000.0, 100000.0));
 //		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.TRINITARIO_MG_NEQ, 
@@ -27,18 +27,16 @@ public class CreateurRomu implements IActeur {
 //				Chocolat.MG_NE_HP, 5000, 1.8, 5000.0, 5000.0, 100000.0, 0.20));
 		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.CRIOLLO_HG_EQ, 
 				Chocolat.HG_E_SHP, 2000, 1.1, 2000.0, 2000.0, 100000.0, 0.25));
-		Monde.LE_MONDE.ajouterActeur(new TransformateurRomu(Feve.FORASTERO_MG_NEQ, 
-				Chocolat.HG_E_SHP, 2000, 1.1, 2000.0, 2000.0, 100000.0, 0.25));
 //		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.MG_NE_HP, 
 //				0.25, 200.0, 100000.0));
-//		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.HG_E_SHP, 
-//				0.35, 200.0, 100000.0));
+		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.HG_E_SHP, 
+				0.35, 200.0, 100000.0));
 	//	Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.MG_NE_HP, 
 	//			0.20, 200.0, 100000.0));
 //		Monde.LE_MONDE.ajouterActeur(new DistributeurRomu(Chocolat.HG_E_SHP, 
 //				0.30, 200.0, 100000.0));
-//		Monde.LE_MONDE.ajouterActeur(new ClientFinalRomu(Chocolat.HG_E_SHP, 
-//				7500.0));
+		Monde.LE_MONDE.ajouterActeur(new ClientFinalRomu(Chocolat.HG_E_SHP, 
+				7500.0));
 //		Monde.LE_MONDE.ajouterActeur(new ClientFinalRomu(Chocolat.MG_NE_HP, 
 //				7500.0));
 	}
